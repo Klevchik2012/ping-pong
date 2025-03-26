ab96f5a118968321968a7c050aeda1de7eb3d230
Klevchik2012 <Klevchik2012@yandex.ru>
2025-03-26 20:51:06 +0300

    столкновение спрайтов

--- main.py
+++ main.py
@@ -16,18 +16,25 @@ display.set_caption("Догонялки")
 player_left = Player(BAGET_IMG,5,(WIN_H - BAGET_H)/2, BAGET_W,BAGET_H)
 # 
 player_right = Player(BAGET_IMG,WIN_W - BAGET_W -5,(WIN_H - BAGET_H)/2, BAGET_W,BAGET_H)
+players = sprite.Group()
+players.add(player_left)
+players.add(player_right)
 cheese = Ball(CHEESE_IMG,(WIN_W - CHEESE_W)/2,(WIN_H - CHEESE_H)/2, CHEESE_W,CHEESE_H)
 # игровой цикл
 game = True
 while game:
     window.fill(LIGHT_BLUE)
 
-    player_left.draw(window)
-    player_right.draw(window)
+    players.draw(window)
     cheese.draw(window)
     player_left.update(K_w,K_s)
     player_right.update(K_UP,K_DOWN)
     cheese.update()
+
+    if sprite.spritecollide(
+            cheese, players, False
+        ):
+        cheese.speed_x *= -1
     # слушать события и обрабатывать
     for e in event.get():
         # выйти, если нажат "крестик"
